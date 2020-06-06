3513095ba14194a0f36ff9baaaaba6ea31bac0e3
waymes <vladimirbar7@gmail.com>
2020-06-06 12:26:32 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const withSass = require("@zeit/next-sass");
+const webpack = require("webpack");
+const { parsed: localEnv } = require("dotenv").config();
+
+module.exports = withSass({
+  distDir: "build",
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000
+        }
+      }
+    });
+
+    return config;
+  }
+});
---
+++ src/server.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: 'src' });
+const handle = app.getRequestHandler();
+const PORT = process.env.PORT || 3000;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
